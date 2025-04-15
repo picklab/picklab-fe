8fa95950b356fd3903ce9b789851329529d398b7
gogleset <choij2494@gmail.com>
2025-04-15 13:11:53 +0900

    🚀 [Feat]: typography text tokenizing화 - tailwind.config 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { fontSizes, fontWeights, lineHeights, letterSpacings } from './src/styles/theme/text';
 import type { Config } from 'tailwindcss';
 import defaultTheme from 'tailwindcss/defaultTheme';
 
@@ -13,42 +14,16 @@ const config: Config = {
         sans: ['Pretendard-Regular', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        background: '#ffffff', // 원하면 여기도 직접 hex 값으로
+        foreground: '#000000',
       },
-      fontSize: {
-        '6xl': 'var(--font-size-6xl)',
-        '4xl': 'var(--font-size-4xl)',
-        '3xl': 'var(--font-size-3xl)',
-        '2xl': 'var(--font-size-2xl)',
-        xl: 'var(--font-size-xl)',
-        lg: 'var(--font-size-lg)',
-        base: 'var(--font-size-base)',
-        sm: 'var(--font-size-sm)',
-        xs: 'var(--font-size-xs)',
-        '2xs': 'var(--font-size-2xs)',
-        '3xs': 'var(--font-size-3xs)',
-        '4xs': 'var(--font-size-4xs)',
-        '5xs': 'var(--font-size-5xs)',
-        '6xs': 'var(--font-size-6xs)',
-        '7xs': 'var(--font-size-7xs)',
-      },
-      fontWeight: {
-        normal: 'var(--font-weight-normal)',
-        medium: 'var(--font-weight-medium)',
-        semibold: 'var(--font-weight-semibold)',
-        bold: 'var(--font-weight-bold)',
-      },
-      lineHeight: {
-        normal: 'var(--line-height-normal)',
-        relaxed: 'var(--line-height-relaxed)',
-      },
-      letterSpacing: {
-        tight: 'var(--letter-spacing-tight)',
-        wide: 'var(--letter-spacing-wide)',
-      },
+      fontSize: fontSizes,
+      fontWeight: fontWeights,
+      lineHeight: lineHeights,
+      letterSpacing: letterSpacings,
     },
   },
   plugins: [],
 };
+
 export default config;
