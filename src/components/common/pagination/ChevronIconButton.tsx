6351eee96ac5ebb60ee70b71c393e3e96b92d175
gogleset <choij2494@gmail.com>
2025-04-17 10:12:43 +0900

    🚑️ [Fix]: Icon import 상대경로 => 절대경로 수정 - rounded-[4px] => rounded 수정

--- src/components/common/pagination/ChevronIconButton.tsx
+++ src/components/common/pagination/ChevronIconButton.tsx
@@ -1,5 +1,5 @@
+import Icon from '@/components/common/Icon/Icon';
 import React, { ButtonHTMLAttributes } from 'react';
-import Icon from '../Icon/Icon';
 
 interface ChevronIconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   // 방향을 나타내는 prop: 'left' 또는 'right' 중 하나
@@ -16,7 +16,7 @@ const ChevronIconButton = ({ direction, ...props }: ChevronIconButtonProps) => {
       // 그룹 클래스를 사용해 내부 아이콘에도 hover/active/disabled 상태를 전파
       // hover 시 밝은 회색 배경, active 시 좀 더 진한 회색 배경
       // disabled 상태일 경우 포인터 이벤트를 막아 클릭 차단
-      className="group size-space-32 border-[1px] border-gray-20 rounded-[4px] p-[3px] hover:bg-gray-5 active:bg-gray-10 disabled:pointer-events-none"
+      className="group size-space-32 border-[1px] border-gray-20 rounded p-[3px] hover:bg-gray-5 active:bg-gray-10 disabled:pointer-events-none"
       {...props}
     >
       {/* 아이콘을 버튼 가운데 정렬하기 위한 flex 컨테이너 */}
