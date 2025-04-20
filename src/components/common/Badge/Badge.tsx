af1b1f5a73936f7b89498c83fac19a4b875f6719
plla2 <jhbljs92@naver.com>
2025-04-20 11:27:19 +0900

    🚑 [Fix]: Badge 컴포넌트 clsx 수정

--- src/components/common/Badge/Badge.tsx
+++ src/components/common/Badge/Badge.tsx
@@ -1,41 +1,25 @@
 import Typography from '@/components/common/Typography';
+import clsx from 'clsx';
 import React from 'react';
 
 interface BadgeProps extends React.HTMLAttributes<HTMLSpanElement> {
   // Badge에 표시할 텍스트
   text: string;
   // Variant 타입 (default, deadline, intended)
-  variant?: 'default' | 'deadline' | 'intended';
+  variant: 'default' | 'deadline' | 'intended';
 }
 
-const Badge = ({ text, variant = 'default', ...props }: BadgeProps) => {
-  // 각 variant에 따라 배경 색상과 텍스트 색상을 설정합니다
-  let bgColor: string;
-  let textColor: string;
+const Badge = ({ text, variant, className, ...props }: BadgeProps) => {
+  const baseClasses = 'w-fit px-space-8 py-space-base rounded-md inline-block cursor-default';
 
-  // variant별로 달라지는 스타일들 swtich문으로 변경합니다
-  switch (variant) {
-    case 'deadline':
-      bgColor = 'bg-danger-50';
-      textColor = 'text-danger-5';
-      break;
-    case 'intended':
-      bgColor = 'bg-gray-10';
-      textColor = 'text-gray-50';
-      break;
-    case 'default':
-    default:
-      bgColor = 'bg-gray-90';
-      textColor = 'text-gray-0';
-      break;
-  }
+  const variantClasses = {
+    default: 'bg-gray-90 text-gray-0',
+    deadline: 'bg-danger-50 text-danger-5',
+    intended: 'bg-gray-10 text-gray-50',
+  };
 
   return (
-    <Typography
-      type="Caption1Medium"
-      className={`${bgColor} ${textColor} w-fit px-space-8 py-space-base rounded-md inline-block cursor-default`}
-      {...props} // 추가적인 옵셔널 span태그의 속성들 적용합니다
-    >
+    <Typography type="Caption1Medium" className={clsx(baseClasses, variantClasses[variant], className)} {...props}>
       {text}
     </Typography>
   );
