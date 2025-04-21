75fae507e2c8174e1810f960a7fb6f2a071e9127
plla2 <jhbljs92@naver.com>
2025-04-21 13:31:56 +0900

    🚑 [Fix]: CheckLabel 컴포넌트에 Label 컴포넌트 사용

--- src/components/common/Check/CheckLabel.tsx
+++ src/components/common/Check/CheckLabel.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CheckBox from './CheckBox';
-import Typography from '@/components/common/Typography';
+import Label from '@/components/common/Label';
 
 interface CheckLabelProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string; // 우측에 표시할 텍스트
@@ -13,14 +13,7 @@ const CheckLabel = ({ label, error, id, ...props }: CheckLabelProps) => {
   return (
     <div className="flex items-center gap-space-10">
       <CheckBox id={checkboxId} error={error} {...props} />
-      <Typography
-        type="Body1Medium"
-        tag="label"
-        htmlFor={checkboxId} // CheckBox 컴포넌트의 id와 동기화
-        className="text-gray-60 cursor-pointer"
-      >
-        {label}
-      </Typography>
+      <Label title={label} htmlFor={checkboxId} />
     </div>
   );
 };
