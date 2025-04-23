88e63a5ffed37846d43dcef673957d7ad2ce342b
plla2 <jhbljs92@naver.com>
2025-04-23 12:59:47 +0900

    🚑 [Fix]: Home 페이지 파일 복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,3 @@
-'use client';
-
-import { iconMap } from '@/components/common/Icon/assets';
-import { OptionGroup } from '@/components/common/OptionGroup/OptionGroup';
-import { useState } from 'react';
-
 export default function Home() {
-  const [selected, setSelected] = useState('label1');
-
-  const options = [
-    { value: 'label1', label: '라벨1' },
-    { value: 'label2', label: '라벨2', icon: 'alertCircle' as keyof typeof iconMap },
-    { value: 'label3', label: '라벨3', icon: 'check' as keyof typeof iconMap },
-  ];
-  return (
-    <div className="text-2xl">
-      <OptionGroup options={options} selectedValue={selected} onSelect={setSelected} />
-    </div>
-  );
+  return <div className="text-2xl">hello</div>;
 }
