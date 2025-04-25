74f04081b45b05deae5f067ed43551e8aefb5f70
plla2 <jhbljs92@naver.com>
2025-04-25 16:48:02 +0900

    🚀 [Feat]: OptionGroup컴포넌트에 width props 추가

--- src/components/common/Field/OptionGroup.tsx
+++ src/components/common/Field/OptionGroup.tsx
@@ -1,5 +1,7 @@
 import CheckBoxLabel from '@/components/common/Field/CheckBoxLabel';
 import Label, { Option } from '@/components/common/Field/Label';
+import { widthClassMap } from '@/components/common/Field/SelectField';
+import clsx from 'clsx';
 import React from 'react';
 
 type OptionGroupProps = {
@@ -7,13 +9,23 @@ type OptionGroupProps = {
   selectedValue: string; // 현재 선택된 값
   onClickHandler: (value: string) => void;
   type?: 'checkBox' | 'default';
+  width?: 'default' | 'large' | 'small';
 };
 
-export const OptionGroup = ({ options, selectedValue, onClickHandler, type = 'default' }: OptionGroupProps) => {
+export const OptionGroup = ({
+  options,
+  selectedValue,
+  onClickHandler,
+  type = 'default',
+  width = 'default',
+}: OptionGroupProps) => {
   return (
     <ul
       role="listbox"
-      className="flex flex-col w-60 mt-1 rounded border border-gray-30 bg-gray-0 p-space-2 focus:outline-none"
+      className={clsx(
+        'flex flex-col mt-1 rounded border border-gray-30 bg-gray-0 p-space-2 focus:outline-none',
+        widthClassMap[width],
+      )}
     >
       {type === 'checkBox' && (
         <CheckBoxLabel
