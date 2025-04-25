99941379eb9eabfb0faeea391b5df081dfb7cf45
plla2 <jhbljs92@naver.com>
2025-04-25 10:00:25 +0900

    🚀 [Feat]: storybook에서만 next/navigation 훅 모킹한 훅 사용

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -12,6 +13,14 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/nextjs',
     options: {},
+  },
+  webpackFinal: async (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      'next/navigation': path.resolve(__dirname, '../src/__mocks__/next/navigation.ts'),
+    };
+    return config;
   },
   staticDirs: ['../public'],
 };
