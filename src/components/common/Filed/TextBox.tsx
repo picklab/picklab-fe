8a677d1414a2aff3da71052d1204de88c7ca80c1
gogleset <choij2494@gmail.com>
2025-04-25 14:06:08 +0900

    🚚 [File]: placeHolderField -> TextBox 로 변경

--- src/components/common/Filed/TextBox.tsx
+++ src/components/common/Filed/TextBox.tsx
@@ -3,10 +3,10 @@
 import clsx from 'clsx';
 import React, { InputHTMLAttributes } from 'react';
 
-export interface PlaceholderFiledProps extends InputHTMLAttributes<HTMLInputElement> {
+export interface TextBoxProps extends InputHTMLAttributes<HTMLInputElement> {
   error?: boolean;
 }
-const PlaceholderFiled = ({ error = false, ...props }: PlaceholderFiledProps) => {
+const TextBox = ({ error = false, ...props }: TextBoxProps) => {
   // error style 제거
   if (props.disabled) {
     error = false;
@@ -15,14 +15,14 @@ const PlaceholderFiled = ({ error = false, ...props }: PlaceholderFiledProps) =>
     <input
       type="text"
       className={clsx(
-        `box-border w-[240px] h-[48px] rounded-md border px-space-12 py-space-8
+        `box-border w-[240px] h-[48px] rounded-md border px-space-12 py-space-8 text-gray-90
           border-interactive-secondary  
           hover:border-gray-50 hover:placeholder:text-gray-50
           active:border-gray-50 
-          disabled:bg-disabled disabled:border-gray-40 disabled:placeholder:text-gray-50 
+          disabled:bg-disabled disabled:border-gray-40 disabled:placeholder:text-gray-50 disabled:cursor-not-allowed
           focus:outline-none
           text-[15px]`,
-        error && '!border-danger-border',
+        error && '!border-danger-border placeholder:!text-gray-90',
       )}
       placeholder="placeholder"
       {...props}
@@ -30,4 +30,4 @@ const PlaceholderFiled = ({ error = false, ...props }: PlaceholderFiledProps) =>
   );
 };
 
-export default PlaceholderFiled;
+export default TextBox;
