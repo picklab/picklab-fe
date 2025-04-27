2e6657b2378ab73b6224bf61996e2a73185d9a71
gogleset <choij2494@gmail.com>
2025-04-28 08:35:27 +0900

    🚀 [Feat]: radius, elevation, colors(dimmed) 값 업데이트

---
+++ src/styles/theme/elevation.ts
@@ -0,0 +1,8 @@
+export const elevation = {
+  // 따로 변수값을 정의해주시지 않아 임의로 나누었습니다!(추후 변경 가능)
+  sm: '0px 1px 2px 0px #0000001F',
+  md: '0px 2px 8px 0px #0000001F',
+  lg: '0px 6px 12px 0px #0000001F',
+  xl: '0px 16px 20px 0px #0000001F',
+  optionGroup: '1px 4px 4.8px 0px #00000021', // 임시
+};
---
+++ src/styles/theme/radius.ts
@@ -0,0 +1,8 @@
+export const radius = {
+  // rounded-full-circle과 none은 기본 클래스로 쓸 수 있어 배제했습니다!
+  ['extra-small']: '4px',
+  small: '6px',
+  medium: '8px',
+  large: '10px',
+  ['extra-large']: '12px',
+};
