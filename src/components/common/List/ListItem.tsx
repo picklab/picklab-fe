a31da97bf378b237de21833fb59153f061b58c61
plla2 <jhbljs92@naver.com>
2025-04-30 14:24:10 +0900

    🚀 [Feat]: ListItem 컴포넌트 작업

---
+++ src/components/common/List/ListItem.tsx
@@ -0,0 +1,151 @@
+'use client';
+
+import Chip from '@/components/common/Card/Chip';
+import Icon from '@/components/common/Icon/Icon';
+import Typography from '@/components/common/Typography';
+import clsx from 'clsx';
+import dayjs from 'dayjs';
+import Image from 'next/image';
+import React from 'react';
+
+export interface ListItemProps {
+  thumbnail: string;
+  label?: string;
+  title: string;
+  saveCount?: number;
+  viewCount?: number;
+  isBookmarked?: boolean;
+  isFinished: boolean;
+  onListClick: () => void;
+  onBookmarkClick?: () => void;
+  chipTitle?: string;
+  organization?: string;
+  startDate?: Date | null;
+  endDate?: Date | null;
+}
+
+const ListItem = ({
+  thumbnail,
+  label,
+  title,
+  saveCount,
+  viewCount,
+  isBookmarked,
+  isFinished,
+  onListClick,
+  onBookmarkClick,
+  chipTitle,
+  organization,
+  startDate,
+  endDate,
+}: ListItemProps) => {
+  const formattedStartDate = startDate ? dayjs(startDate).format('YY.MM.DD') : '';
+  const formattedEndDate = endDate ? dayjs(endDate).format('YY.MM.DD') : '';
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      e.preventDefault();
+      onListClick();
+    }
+  };
+
+  // 모집 및 활동이 종료된 FinisihedListItem
+  const renderFinishedContent = () =>
+    chipTitle &&
+    organization &&
+    startDate &&
+    endDate && (
+      <div className="flex flex-col justify-center max-w-[268px] gap-space-8">
+        <div className="flex flex-col gap-space-6">
+          <Chip text={chipTitle} className="cursor-pointer" />
+          <div className="flex flex-col">
+            <Typography type="Body2Semibold" className="text-gray-90 group-active:text-gray-40 truncate">
+              {title}
+            </Typography>
+            <Typography type="Body4Medium" className="text-gray-50 group-active:text-gray-40 truncate">
+              {organization}
+            </Typography>
+          </div>
+        </div>
+        <div className="flex items-center gap-space-6">
+          <Typography type="Caption1Regular" className="text-gray-90">
+            활동기간
+          </Typography>
+          <Typography type="Caption1Regular" className="text-gray-90">
+            {`${formattedStartDate}~${formattedEndDate}`}
+          </Typography>
+        </div>
+      </div>
+    );
+
+  // 모집 및 활동이 진행중인 ListItem
+  const renderOngoingContent = () =>
+    label &&
+    saveCount &&
+    viewCount && (
+      <div className="flex flex-col gap-space-base py-space-base max-w-[268px]">
+        <div className="flex flex-col gap-space-2">
+          <Typography type="Body4Medium" className="text-gray-50 group-active:text-gray-40 truncate">
+            {label}
+          </Typography>
+          <Typography type="Body1Semibold" className="text-gray-90 group-active:text-gray-40 truncate">
+            {title}
+          </Typography>
+        </div>
+        <div className="flex gap-space-14 items-center">
+          <div className="flex gap-space-2 items-center">
+            <Typography type="Caption1Medium" className="text-gray-50 group-active:text-gray-40">
+              저장수
+            </Typography>
+            <Typography type="Caption1Medium" className="text-gray-50 group-active:text-gray-40">
+              {saveCount}
+            </Typography>
+          </div>
+          <div className="flex gap-space-2 items-center">
+            <Typography type="Caption1Medium" className="text-gray-50 group-active:text-gray-40">
+              조회수
+            </Typography>
+            <Typography type="Caption1Medium" className="text-gray-50 group-active:text-gray-40">
+              {viewCount}
+            </Typography>
+          </div>
+        </div>
+      </div>
+    );
+
+  return (
+    <div
+      className={clsx(
+        'flex justify-between items-center cursor-pointer border-b border-gray-20 focus:outline-none',
+        isFinished ? 'py-space-base' : 'py-space-14',
+      )}
+      role="button"
+      tabIndex={0}
+      onClick={onListClick}
+      onKeyDown={handleKeyDown}
+    >
+      <div className={clsx('flex gap-space-14 items-start group', isFinished ? 'min-w-[368px]' : 'min-w-[488px]')}>
+        <div className={clsx('w-[86px] relative overflow-hidden', isFinished ? 'h-[94px]' : 'h-[86px]')}>
+          <Image src={thumbnail} alt="리스트 썸네일" sizes="86" fill className="object-cover rounded-lg" />
+        </div>
+        {isFinished ? renderFinishedContent() : renderOngoingContent()}
+      </div>
+      {!isFinished && (
+        <Icon
+          icon={isBookmarked ? 'bookmarkFill' : 'bookmarkLine'}
+          size={24}
+          className={clsx('cursor-pointer outline-none', isBookmarked ? 'text-primary-50' : 'text-gray-40')}
+          onClick={(e) => {
+            e.stopPropagation();
+            onBookmarkClick?.();
+          }}
+          aria-pressed={isBookmarked}
+          role="button"
+          tabIndex={0}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ListItem;
