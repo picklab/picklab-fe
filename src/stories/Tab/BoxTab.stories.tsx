36f6fe5ab641c92a9e1ef3be12a8323f71c947e5
gogleset <choij2494@gmail.com>
2025-05-01 15:31:44 +0900

    🚑 [Fix]: BoxTab stories 렌더링 방식 수정

--- src/stories/Tab/BoxTab.stories.tsx
+++ src/stories/Tab/BoxTab.stories.tsx
@@ -12,40 +12,42 @@ const meta: Meta<typeof BoxTab> = {
 export default meta;
 type Story = StoryObj<typeof BoxTab>;
 
-export const Default: Story = {
-  render: () => {
-    const [activeTab, setActiveTab] = useState('tab1');
+const BoxTabWithState = () => {
+  const [activeTab, setActiveTab] = useState('tab1');
 
-    return (
-      <div role="tablist" className="flex">
-        <BoxTab
-          label="전체"
-          notiNumber="12"
-          href="#"
-          id="tab1"
-          panelId="panel1"
-          active={activeTab === 'tab1'}
-          onClick={() => setActiveTab('tab1')}
-        />
-        <BoxTab
-          label="공지"
-          notiNumber="3"
-          href="#"
-          id="tab2"
-          panelId="panel2"
-          active={activeTab === 'tab2'}
-          onClick={() => setActiveTab('tab2')}
-        />
-        <BoxTab
-          label="질문"
-          notiNumber="8"
-          href="#"
-          id="tab3"
-          panelId="panel3"
-          active={activeTab === 'tab3'}
-          onClick={() => setActiveTab('tab3')}
-        />
-      </div>
-    );
-  },
+  return (
+    <div role="tablist" className="flex">
+      <BoxTab
+        label="전체"
+        notiNumber="12"
+        href="#"
+        id="tab1"
+        panelId="panel1"
+        active={activeTab === 'tab1'}
+        onClick={() => setActiveTab('tab1')}
+      />
+      <BoxTab
+        label="공지"
+        notiNumber="3"
+        href="#"
+        id="tab2"
+        panelId="panel2"
+        active={activeTab === 'tab2'}
+        onClick={() => setActiveTab('tab2')}
+      />
+      <BoxTab
+        label="질문"
+        notiNumber="8"
+        href="#"
+        id="tab3"
+        panelId="panel3"
+        active={activeTab === 'tab3'}
+        onClick={() => setActiveTab('tab3')}
+      />
+    </div>
+  );
+};
+
+export const Default: Story = {
+  render: () => <BoxTabWithState />,
 };
