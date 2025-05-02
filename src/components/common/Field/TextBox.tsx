d393cf3d3d3f53e1f411e451c356a88f9f2ec131
gogleset <choij2494@gmail.com>
2025-05-02 14:41:21 +0900

    🚀 [Feat]: TextBox scale, rounded, icon props 추가

--- src/components/common/Field/TextBox.tsx
+++ src/components/common/Field/TextBox.tsx
@@ -1,34 +1,48 @@
 'use client';
 
+import { IconType } from '@/components/common/Icon/assets';
+import Icon from '@/components/common/Icon/Icon';
 import clsx from 'clsx';
 import React, { InputHTMLAttributes } from 'react';
 
+const scaleStyleClass = {
+  base: 'h-space-48 px-[18px] py-[13px]',
+  sm: 'h-space-34 px-[18px] py-[7px]',
+};
 export interface TextBoxProps extends InputHTMLAttributes<HTMLInputElement> {
   error?: boolean;
+  rounded?: boolean;
   className?: string;
+  scale?: keyof typeof scaleStyleClass;
+  icon?: IconType;
 }
-const TextBox = ({ error = false, className, ...props }: TextBoxProps) => {
+
+const TextBox = ({ error = false, rounded = false, scale = 'base', icon, className, ...props }: TextBoxProps) => {
   // error style 제거
   if (props.disabled) {
     error = false;
   }
   return (
-    <input
-      type="text"
-      className={clsx(
-        `box-border w-60 h-space-48 rounded-md border px-space-12 py-space-8 text-gray-90
+    <div className="relative w-fit">
+      <input
+        type="text"
+        className={clsx(
+          `box-border w-60  rounded-md border text-gray-90
           border-interactive-secondary  
           hover:border-gray-50 hover:placeholder:text-gray-50
           active:border-gray-50 
           disabled:bg-disabled disabled:border-gray-40 disabled:placeholder:text-gray-50 disabled:cursor-not-allowed
           focus:outline-none
           text-[15px]`,
-        error && '!border-danger-border placeholder:!text-gray-90',
-        className,
-      )}
-      placeholder="placeholder"
-      {...props}
-    />
+          error && '!border-danger-border placeholder:!text-gray-90',
+          rounded && '!rounded-full',
+          scale && scaleStyleClass[scale],
+        )}
+        placeholder="placeholder"
+        {...props}
+      />
+      {icon && <Icon className={clsx('absolute top-1/2 -translate-y-1/2 right-[16px]')} icon={icon} size={24} />}
+    </div>
   );
 };
 
