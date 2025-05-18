429be69adc3aa4bccc17b9d815f521728269a812
gogleset <choij2494@gmail.com>
2025-05-18 12:27:29 +0900

    🚑 [Fix]: 잘못된 viewbox 값 수정

--- src/components/common/Icon/assets/Large/LargeChevronDown.tsx
+++ src/components/common/Icon/assets/Large/LargeChevronDown.tsx
@@ -5,7 +5,7 @@ const LargeChevronDown = ({ width, height, className, ...props }: React.SVGProps
     <svg
       width={width}
       height={height}
-      viewBox="0 0 11 19"
+      viewBox="0 0 19 11"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={className}
