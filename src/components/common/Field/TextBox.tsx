14c4d4676136430c6fd85043eeed5135440d7a96
gogleset <choij2494@gmail.com>
2025-05-19 16:23:41 +0900

    🚑 [Fix]: TextBox 스타일 수정, Search 컴포넌트 className 적용되게 수정

--- src/components/common/Field/TextBox.tsx
+++ src/components/common/Field/TextBox.tsx
@@ -28,7 +28,7 @@ const TextBox = ({ error = false, rounded = false, scale = 'base', icon, classNa
         type="text"
         className={clsx(
           `box-border w-60  rounded-md border text-gray-90
-          border-interactive-secondary  
+          border-gray-30  
           hover:border-gray-40 hover:placeholder:text-gray-50
           active:border-gray-50 
           disabled:bg-gray-5 disabled:border-gray-30 disabled:placeholder:text-gray-40 disabled:cursor-not-allowed
