e489cb9f50eed5757ac757784fa1df2c0214d729
gogleset <choij2494@gmail.com>
2025-05-24 09:39:47 +0900

    🚀 [Feat]: GNB, SNB 스토리북 작성

--- src/stories/GNB/GNB.stories.tsx
+++ src/stories/GNB/GNB.stories.tsx
@@ -9,29 +9,8 @@ const meta: Meta<typeof GNB> = {
   },
   decorators: [
     (Story) => (
-      //   <div
-      //     style={{
-      //       scale: 0.85,
-      //       width: '1440px',
-      //       maxWidth: '100%',
-      //       transform: 'translateX(-260px)',
-      //       overflowX: 'scroll', //  <-- 이 부분을 추가하세요.
-      //       // 필요하다면 overflowY도 설정할 수 있습니다. 예: overflowY: 'hidden'
-      //     }}
-      //   >
-      //     <Story />
-      //   </div>
-      <div
-        style={{
-          width: '300px',
-          maxWidth: '300px',
-          overflowX: 'scroll',
-          border: '1px solid red',
-        }}
-      >
-        <div style={{ width: '1000px', background: '#eee' }}>
-          <Story />
-        </div>
+      <div className="scale-[0.85] w-[1440px] -translate-x-[230px]">
+        <Story />
       </div>
     ),
   ],
