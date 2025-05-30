b8cc60fcc514009bd6d5e9ddcca9eae70e401a12
gogleset <choij2494@gmail.com>
2025-05-30 10:58:27 +0900

    🚀 [Feat]: TextArea maxLength, minLength 기능 추가

--- src/components/common/Field/TextArea.tsx
+++ src/components/common/Field/TextArea.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import HelpMessage, { HelpMessageProps } from '@/components/common/Field/HelpMessage';
 import LabelType from '@/components/common/Field/LabelType';
 import TextBox, { TextareaTextBoxProps } from '@/components/common/Field/TextBox';
@@ -7,20 +8,33 @@ import Typography from '@/components/common/Typography';
 import clsx from 'clsx';
 import React, { ChangeEvent, useState } from 'react';
 
+// TextArea 컴포넌트의 props 타입 정의
 export type TextAreaProps = Omit<TextareaTextBoxProps, 'error' | 'textBoxType'> &
   WithOptionalLabel & {
     helpMessage?: string;
-    status: HelpMessageProps['status'];
+    status: HelpMessageProps['status']; // 상태 (예: 'error', 'success')
+    maxLength: number;
+    minLength?: number;
   };
-const TextArea = (props: TextAreaProps) => {
-  const { label, id, helpMessage, status, labelStatus, disabled, maxLength, ...rest } = props;
-  const [length, setLength] = useState(0);
 
+// TextArea 컴포넌트
+const TextArea = (props: TextAreaProps) => {
+  const { label, id, status, labelStatus, helpMessage, minLength, disabled, maxLength, ...rest } = props;
+
+  const lengthMessageClass = 'h-space-12';
+
+  const [length, setLength] = useState(0); // 입력된 글자 수 상태
+  const isMaxLength = length > maxLength; // 최대 글자를 초과했나 판별하는 변수
+  const isUnderMinLength = minLength ? length < minLength : false; // 최소 글자인지 판별하는 변수
+  const helpMessageTitle = isUnderMinLength ? `최소 ${minLength}글자 이상 입력하세요` : helpMessage;
+  // textarea 값이 변경될 때 글자 수 업데이트
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setLength(e.target.value.length);
   };
+
   return (
     <div className="flex flex-col w-fit h-fit gap-1">
+      {/* 라벨 영역 */}
       {label && (
         <LabelType
           disable={disabled}
@@ -30,33 +44,35 @@ const TextArea = (props: TextAreaProps) => {
           className={clsx(disabled && '!cursor-not-allowed')}
         />
       )}
+
+      {/* 텍스트박스 영역 */}
       <TextBox
         id={id}
         disabled={disabled}
         {...rest}
-        error={status === 'error'}
+        error={isMaxLength || status === 'error'}
         textBoxType="textarea"
         maxLength={maxLength}
-        className="!rounded"
+        minLength={minLength}
         onChange={(event: ChangeEvent<HTMLTextAreaElement>) => {
-          handleChange(event);
-          if (rest.onChange) rest.onChange(event);
+          handleChange(event); // 글자 수 업데이트
+          rest.onChange?.(event); // 외부 onChange 호출
         }}
       />
-      {(helpMessage || maxLength) && (
-        <div className="flex justify-between items-center text-gray-60 px-1">
-          {helpMessage ? (
-            <HelpMessage className="mt-0.5" title={helpMessage} status={status} />
-          ) : (
-            <div /> // 공간 확보용
-          )}
-          {maxLength && (
-            <Typography type="Caption2Semibold" className={clsx(length >= maxLength && 'text-danger-50', 'self-end')}>
-              {length} / {maxLength}
-            </Typography>
-          )}
+
+      {/* 하단 안내 메시지 및 글자 수 카운터 */}
+      <div className={clsx('flex justify-between items-center')}>
+        {helpMessageTitle ? <HelpMessage status={disabled ? 'disabled' : status} title={helpMessageTitle} /> : <div />}
+        {/* 글자 수 카운터 */}
+        <div className={clsx('flex items-center text-gray-60', disabled && '!text-gray-40')}>
+          <Typography type="Caption2Medium" className={clsx(lengthMessageClass, isMaxLength && 'text-danger-50')}>
+            {length}&nbsp;
+          </Typography>
+          <Typography type="Caption2Medium" className={lengthMessageClass}>
+            / {maxLength}
+          </Typography>
         </div>
-      )}
+      </div>
     </div>
   );
 };
