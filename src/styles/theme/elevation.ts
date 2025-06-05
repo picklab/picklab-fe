646a9bc8ac701bbc9d896341f06289c06f52bc3b
gogleset <choij2494@gmail.com>
2025-06-05 11:46:15 +0900

    🚀 [Feat]: elevation optionGroup 그림자 토큰 값 수정

--- src/styles/theme/elevation.ts
+++ src/styles/theme/elevation.ts
@@ -4,5 +4,5 @@ export const elevation = {
   md: '0px 2px 8px 0px #0000001F',
   lg: '0px 6px 12px 0px #0000001F',
   xl: '0px 16px 20px 0px #0000001F',
-  optionGroup: '1px 4px 4.8px 0px #00000021', // 임시
+  optionGroup: '1px 4px 4.8px 0px rgba(0, 6, 12, 0.08)',
 };
