28f372b462a36cc41bc697161c8419e5fb6a4e5b
gogleset <choij2494@gmail.com>
2025-06-12 10:20:17 +0900

    🚑 [Fix]: Field => TextField로 수정

--- src/stories/TextField/HelpMessage.stories.tsx
+++ src/stories/TextField/HelpMessage.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import HelpMessage from '@/components/common/Field/HelpMessage';
 
 const meta = {
-  title: 'Components/Field/HelpMessage',
+  title: 'Components/TextField/HelpMessage',
   component: HelpMessage,
   tags: ['autodocs'],
   argTypes: {
