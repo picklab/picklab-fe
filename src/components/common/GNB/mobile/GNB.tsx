f9f6668152edacf115d5435fb850f3d0fb1e079d
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-07-31 19:01:18 +0900

    Feat[GNB-mobile]: 모바일 GNB 컴포넌트 및 스토리북 추가, CowBell 아이콘 추가

---
+++ src/components/common/GNB/mobile/GNB.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Icon from '../../Icon/Icon';
+import Avatar from '../Avatar';
+import Button from '../../Button/Button';
+
+interface GNBProps {
+  isLogin: boolean;
+}
+
+const GNB = ({ isLogin = false }: GNBProps) => {
+  return (
+    <header className="w-full max-w-[23.4375rem] h-[3.625rem] bg-gray-0 px-5 flex items-center justify-between ">
+      <Link href="/" aria-label="PickLab 홈">
+        <Image src="/imgs/logo_mobile.png" width={93} height={22} alt="PickLab 로고" />
+      </Link>
+
+      {isLogin ? (
+        <nav aria-label="유저 메뉴">
+          <ul className="flex items-center gap-[0.625rem]">
+            <li>
+              <Link href="/calendar" aria-label="캘린더">
+                <Icon size={24} icon="calendar" />
+              </Link>
+            </li>
+            <li>
+              <Link href="/notes" aria-label="메모">
+                <Icon size={24} icon="pencil" />
+              </Link>
+            </li>
+            <li>
+              <Link href="/notifications" aria-label="알림">
+                <Icon size={24} icon="cowBell" />
+              </Link>
+            </li>
+            <li>
+              <Link href="/profile" aria-label="프로필">
+                <Avatar />
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <Button label="로그인" buttonStyle="outlined" size="sm" className="w-[3.125rem] h-[1.625rem] !px-2" />
+      )}
+    </header>
+  );
+};
+
+export default GNB;
