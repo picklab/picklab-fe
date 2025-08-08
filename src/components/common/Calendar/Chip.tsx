5fd40555a359fcac139e62bf35f7830f95dcb609
gogleset <choij2494@gmail.com>
2025-08-08 19:50:33 +0900

    🚀 [Feat]: Calendar용 Chip 컴포넌트 추가

---
+++ src/components/common/Calendar/Chip.tsx
@@ -0,0 +1,30 @@
+import Typography from '@/components/common/Typography';
+import clsx from 'clsx';
+import React from 'react';
+
+interface ChipProps extends React.HTMLAttributes<HTMLDivElement> {
+    text: string;
+    period: "start" | "deadline";
+}
+
+const chipStyle = {
+    start: {
+        bg: "bg-primary-10",
+        text: "text-primary-60",
+    },
+    deadline: {
+        bg: "bg-danger-5",
+        text: "text-danger-40",
+    },
+}
+
+const Chip = ({ text, className, period = "start", ...props }: ChipProps) => {
+    
+    return (
+        <div className={clsx("flex w-[33px] h-5 items-center px-1.5 py-.5 rounded",chipStyle[period].bg, className)} {...props}>
+            <Typography type="Caption1Semibold" className={chipStyle[period].text}>{text}</Typography>
+        </div>
+    );
+};
+
+export default Chip;
