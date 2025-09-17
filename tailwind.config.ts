0b2543749bf88fe78662c9565977ff3849871605
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-09-18 00:11:36 +0900

    Feat[tailwind]: breakpoints for mobile and pc 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        mobile: { max: '1439px' },
+        pc: { min: '1440px' },
+      },
       fontFamily: {
         sans: ['Pretendard-Regular', ...defaultTheme.fontFamily.sans],
       },
