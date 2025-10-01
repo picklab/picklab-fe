f4cd073e2da43b9cb10b49c116bdae86e5e7a150
GanziMan <qjatn50089@gmail.com>
2025-10-01 23:59:42 +0900

    Feat[auth]: create layout and authentication page

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+export default function HomeLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen">
+      {/* Main Content */}
+      <main className="mx-auto pc:max-w-[1100px] mobile:max-w-[335px] mb-10">{children}</main>
+    </div>
+  );
+}
