26f95a180b4d211c02ba807d9b95981e3a038c09
bumsu_kim <bumsu_kim@daekyo.co.kr>
2026-02-04 01:03:47 +0900

    fix: storybook ignore

--- src/stories/Tab/BoxTab.stories.tsx
+++ src/stories/Tab/BoxTab.stories.tsx
@@ -1,19 +1,21 @@
-import BoxTab from '@/components/common/Tab/BoxTab';
-import type { Meta, StoryObj } from '@storybook/react';
+/** @format */
 
-import { useState } from 'react';
+import BoxTab from "@/components/common/Tab/BoxTab";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { useState } from "react";
 
 const meta: Meta<typeof BoxTab> = {
-  title: 'Components/Tab/BoxTab',
+  title: "Components/Tab/BoxTab",
   component: BoxTab,
-  tags: ['autodocs'],
+  tags: ["autodocs"],
 };
 
 export default meta;
 type Story = StoryObj<typeof BoxTab>;
 
 const BoxTabWithState = () => {
-  const [activeTab, setActiveTab] = useState('tab1');
+  const [activeTab, setActiveTab] = useState("tab1");
 
   return (
     <div role="tablist" className="flex">
@@ -23,26 +25,26 @@ const BoxTabWithState = () => {
         href="#"
         id="tab1"
         panelId="panel1"
-        active={activeTab === 'tab1'}
-        onClick={() => setActiveTab('tab1')}
+        active={activeTab === "tab1"}
+        onClick={() => setActiveTab("tab1")}
       />
       <BoxTab
-        label="공지"
+        label="강연/세미나"
         notiNumber="3"
         href="#"
         id="tab2"
         panelId="panel2"
-        active={activeTab === 'tab2'}
-        onClick={() => setActiveTab('tab2')}
+        active={activeTab === "tab2"}
+        onClick={() => setActiveTab("tab2")}
       />
       <BoxTab
-        label="질문"
+        label="교육"
         notiNumber="8"
         href="#"
         id="tab3"
         panelId="panel3"
-        active={activeTab === 'tab3'}
-        onClick={() => setActiveTab('tab3')}
+        active={activeTab === "tab3"}
+        onClick={() => setActiveTab("tab3")}
       />
     </div>
   );
